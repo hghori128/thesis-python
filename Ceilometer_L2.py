78e8d24d969288b31318dde018ccbda75a55109e
Hannan Ghori <hghori@uwo.ca>
2021-01-15 18:32:31 -0500

    L2 WORKING norm,avg signal plot (like L0 data)

--- Ceilometer_L2.py
+++ Ceilometer_L2.py
@@ -1,88 +1,122 @@
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+import numpy.matlib
 
+import pandas as pd
+import netCDF4 
 
 import xarray as xr
 
+# Iniatalize coadd function to be used below 
+
+def coadd(q,z,layer):
+
+    l = math.floor(len(q) / layer) * layer
+    q = q[1:l+1]
+    z = z[1:l+1]
+    # Reshape q and z so that the bins from each layer are in the
+    # same column
+    qc = np.reshape(q, (layer,int(l/layer)), order='F')
+    zc = np.reshape(z, (layer,int(l/layer)), order='F')
+    qc = (np.mean(qc,0))
+    zc = (np.mean(zc,0))
+    print(np.shape(qc))
+    return [qc, zc]
 #%%
 #----------------------------
 #Following function recieved from http://xarray.pydata.org/en/stable/io.html and modified as such
 # Which should work sufficiently well for large datasets
-from glob import glob
+#-REMOVED-
 
-def read_netcdfs(files, dim, transform_func=None):
-    def process_one_path(path):
-        # use a context manager, to ensure the file gets closed after use
-        with xr.open_dataset(path) as ds:
-            # transform_func should do some sort of selection or
-            # aggregation
-            if transform_func is not None:
-                ds = transform_func(ds)
-            # load all data from the transformed dataset, to ensure we can
-            # use it after closing each original file
-            ds.load()
-            return ds
 
-    paths = sorted(glob(files))
-    datasets = [process_one_path(p) for p in paths]
-    combined = xr.concat(datasets, dim)
-    return combined
 
-# here we suppose we only care about the combined mean of each file;
-# you might also use indexing operations like .sel to subset datasets
-dataset = read_netcdfs('/Users/hannanghori/Documents/university shit/4th year/Thesis/Cronyn/L2/2017/07/30/L2_*.nc', dim='time',transform_func=lambda ds: ds.mean())
-#print(combined)
-beta_att = dataset.variables['beta_att_raw']
-print(beta_att)
+
+
+#-------XARRAY LOAD FILES---------------------
+
+datatest = xr.open_mfdataset('/Users/hannanghori/Documents/university shit/4th year/Thesis/Cronyn/L2/2020/04/01/L2_*.nc', concat_dim="time", data_vars='minimal', coords='minimal', compat='override')
+
+beta_att = datatest.variables['attenuated_backscatter_0']
+range = datatest.variables['altitude']
+station_alt = datatest.variables['station_altitude']
+
+time = datatest.variables['time']
+
+#lt.plot(beta_att, Range)
+
+print(np.shape(beta_att))
+print(np.shape(range))
+print(np.shape(time))
+
+plt.scatter(beta_att[0:512], range)
+
+
 
 
 # %%
-import xarray as xr
+# Attempting to use the xarray dataframes to make a normalized, averaged signal plot
+# did not end up working correctly so see below
 
-xr.open_mfdataset('/Users/hannanghori/Documents/university shit/4th year/Thesis/Cronyn/L2/2017/07/30/L2_*.nc')
+zenith2 = 89
+print(zenith2)
+plotalt = np.matlib.repmat(range*np.sin(zenith2)+station_alt, len(time), 1)
 
-dataset =  xr.open_mfdataset('/Users/hannanghori/Documents/university shit/4th year/Thesis/Cronyn/L2/2017/07/30/L2_*.nc', concat_dim="time", data_vars='minimal', coords='minimal', compat='override')
+a = int(len(beta_att_test)/2)
+print(a)
+d_day = beta_att.iloc[:,a:a*2] 
+d_daysum = np.mean(d_day,1)
+print(d_daysum)
 
-#%%
+[x,y] = coadd(d_daysum,range,10)
+#x = np.abs(x)
+#logprofile = np.log(x)
 
-#----------------------------
-#Following function recieved from http://xarray.pydata.org/en/stable/io.html and modified as such
-
-from glob import glob
-
-def read_netcdfs(files, dim, transform_func=None):
-    def process_one_path(path):
-        # use a context manager, to ensure the file gets closed after use
-        with xr.open_dataset(path) as ds:
-            # transform_func should do some sort of selection or
-            # aggregation
-            if transform_func is not None:
-                ds = transform_func(ds)
-            # load all data from the transformed dataset, to ensure we can
-            # use it after closing each original file
-            ds.load()
-            return ds
-
-    paths = sorted(glob(files))
-    datasets = [process_one_path(p) for p in paths]
-    combined = xr.concat(datasets, dim)
-    return combined
-
-# here we suppose we only care about the combined mean of each file;
-# you might also use indexing operations like .sel to subset datasets
-#%%
-combined = read_netcdfs('/Users/hannanghori/Documents/university shit/4th year/Thesis/Cronyn/L2/2017/07/30/L2_*.nc', dim='time',transform_func=lambda ds: ds.mean())
-print(combined)
-datatest = xr.open_mfdataset('/Users/hannanghori/Documents/university shit/4th year/Thesis/Cronyn/L2/2017/07/30/L2_*.nc', concat_dim="time", data_vars='minimal', coords='minimal', compat='override')
-
-beta_att = combined.variables['attenuated_backscatter_0']
-Range = datatest.variables['altitude']
-
-plt.plot(beta_att, Range)
+# %%
+#----------------------
+# Attempting using the netcdf4 module instead of xarray 
+# This should be easier as this will load in a numpy like array BUT
+# does not handle multiple datasets in the netcdf4 format
 
 
 
-#%%
+t = netCDF4.Dataset("/Users/hannanghori/Documents/university shit/4th year/Thesis/Cronyn/L2/2020/04/01/L2_0-20000-0-73009_A20200401.nc")
+beta_att_test = t.variables['attenuated_backscatter_0']
+range_test = t.variables['altitude']
+time_test = t.variables['time']
+
+plt.plot(beta_att_test, range_test)
+
+
+
+# %%
+#-------------------------WORKING--------------------
+# Make night averaged, normalized plot in a similar fashion to the 
+# way that it is created for the L0 data. 
+# The further corrections done to beta_raw to obtain beta_att should allow 
+# for the same manipulation to normalize and average 
+
+
+overlap = np.loadtxt('data.txt',dtype = float)
+print(overlap)
+overlap_0 = np.resize(overlap,[511,2])
+overlap_atten = overlap_0[:, 0]
+
+a = int(len(beta_att_test)/2)
+print(a)
+d_night_test = beta_att_test[:,a:a*2] 
+d_nightsum_test = np.mean(d_night_test,1)
+for f in overlap_atten:
+  dens_overlap_test = d_nightsum_test * overlap_atten
+print(dens_overlap_test)
+
+print(d_nightsum_test)
+
+[x,y] = coadd(dens_overlap_test,range_test,5)
+plt.semilogx(x[0:55],y[0:55])
+plt.xlabel('Normalized, Range-Corrected Attenuated Backscatter')
+plt.ylabel('Height (m)')
+plt.title('Night Average Signal: 2020/04/01')
 
 # %%
